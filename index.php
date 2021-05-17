ca17b96f62126a3a940d599c2a80064ea380de47
josechableca <pepechable@hotmail.com>
2021-05-17 11:40:40 -0500

    modify

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ require_once('setasign/Fpdi/autoload.php');
 //require_once('setasign/Fpdi/Fpdi.php');
 require_once('setasign/fpdi_pdf-parser2/src/autoload.php');
 
-//$pdf = new /setasign/Fpdi/Fpdi();
 
 date_default_timezone_set('UTC');
 //separador de ruta /  \
@@ -15,9 +14,9 @@ $rut = chr(47); // chr(47)/ chr(92)\
 //numero de ceros en nombre 00000
 $length = 5;
 
-// directorio base
+// directorios fijos
 $dir = array(
-    '/pruebas/carpetaPHP17'
+    '/pruebas/sepatarePDF/carpetaB'
 );
 
 foreach ($dir as $paths) {
@@ -25,12 +24,15 @@ foreach ($dir as $paths) {
     buscarpdf($paths);
 }
 
-/*
+/* se usan los parametros de linea de comando
 if ($argc > 1) {
     $Numinicial = 1;
-    $dir = $argv;
-    
-    for ($i = $Numinicial; $i <= $argc; $i++) { //each ($dir as $paths) {
+    $primero = strtoupper($argv[0]);
+    if ($primero == "PHP") {
+        $Numinicial = 2;
+    }
+        
+    for ($i = $Numinicial; $i <= $argc; $i++) {
 
         buscarpdf($argv[$i]);
     }
@@ -62,59 +64,54 @@ function buscartexto($origen, $texto)
 
 function buscarpdf($path)
 {
-
     $afiles = array();
     $adir = array();
     $nfile = 0;
     $ndir = 0;
     $length = $GLOBALS["length"];
-    $noprocesado = false;
-    $elimnar = false;
+    $procesar = true;
+    $
+    
 
     // Arreglo con todos los nombres de los archivos
     $files = array_diff(scandir($path), array('.', '..'));
-    echo 'Ruta: ' . $path . '<br>' . chr(10);
+    echo 'Ruta: ' . $path . chr(10);
     //listamos los archivos que no contengan la clave _P00000 y a parte la lista de directorios
     foreach ($files as $file) {
-        $noprocesado = false;
+        $procesar = true;
         $data = explode(".", $file);
         //si es un archivo
         if (is_file($path . '/' . $file)) {
             $fileExtension = $data[1];
-            //que sea un pdf
+            //es un archivo pdf
             if (strtoupper($fileExtension) == 'PDF') {
-                $numfin = $length + 2;
-                $final = substr($data[0], -$numfin);
-                $part1 = substr($final, 0, 2);
-                $part2 = substr($final, -$length);
-                $valpart2 = intval($part2);
+                //busca si tiene la clave _P
                 $indexb = buscartexto($data[0], '_P');
                 if ($indexb > 0) {
                     $largo = strlen($data[0]) - $indexb - 2;
+                    //despues de la clave hay caracteres
                     if ($largo > 0 && $largo <= $length) {
+                        //obtenemos la parte numerica
                         $part2 = substr($data[0], $indexb + 2, $largo);
                         $valpart2 = intval($part2);
+                        //es un valor 1 a 99999
                         if ($valpart2 > 0 && $valpart2 < 100000) {
-                            $string1 = substr(str_repeat(0, $length) . '1', -$length);
-                            $parte1 = $path . '/' . $data[0] . '_P' . $string1 . ".pdf";
-                            $parte2 = $path . '/' . $data[0] . '_P1' . ".pdf";
-                            if (file_exists($parte1) || file_exists($parte2)) {
-                                $noprocesado = false;
-                            } else {
-                                $noprocesado = true;
-                            }
-                        } else {
-                            $noprocesado = true;
+                            $procesar = false;
                         }
-                    } else {
-                        $noprocesado = true;
                     }
                 } else {
-                    $noprocesado = true;
+                    $string1 = substr(str_repeat(0, $length) . '1', -$length);
+                    $parte1 = $path . '/' . $data[0] . '_P' . $string1 . ".pdf";
+                    $parte2 = $path . '/' . $data[0] . '_P1' . ".pdf";
+                    if (file_exists($parte1) || file_exists($parte2)) {
+                        $procesar = false;
+                        echo $file . ' -Archivo ya procesado-' . chr(10);
+                    } else {
+                        $procesar = true;
+                    }
                 }
-                //no tiene la clave _P00000
-                //if (!($part1 == '_P' && ($valpart2 > 0 && $valpart2 < 100000))) {
-                if ($noprocesado) {
+
+                if ($procesar) {
                     array_push($afiles, $file);
                     $nfile += 1;
                 }
@@ -128,24 +125,20 @@ function buscarpdf($path)
     //procesamos primero los archivos del directorio
     if ($nfile > 0) {
         foreach ($afiles as $file) {
-            $eliminar = false;
-            // Divides en dos el nombre de tu archivo utilizando el .
             $data = explode(".", $file);
             // Nombre del archivo
             $fileName = $data[0];
             $fileExtension = $data[1];
 
-            echo $file . '  ';
-
-            /*$old_pdf = $fileName;
-            $new_pdf = $path . '/' . $file;*/
+            echo $file . ' ';
 
             $old_pdf = $path . '/' . $file;
             $new_pdf = $path . '/' . $fileName . '_P';
 
             $oldpath = $new_pdf;
 
-            /*            $newpath = verifyPdfVersion($oldpath);
+            /*verifica la version y genera un archivo convertido nombre_v14.pdf
+            $newpath = verifyPdfVersion($oldpath);
             if ($newpath) {
                 if ($newpath != $oldpath) {
                     $new_pdf = $newpath;
@@ -153,20 +146,8 @@ function buscarpdf($path)
                 }
             }*/
 
-            $ahora = DateTime::createFromFormat('U.u', number_format(microtime(true), 6, '.', ''));
-            $formateado = $ahora->format("Y-m-d H:i:s.u");
-            echo 'Inicia division' . $formateado . ' -> ';
-
             dividerPdf($path, $new_pdf, $old_pdf);
-
-            /*if ($eliminar) {
-                shell_exec('rm '.chr(39).$newpath.chr(39));
-            }*/
-
-            $ahora = DateTime::createFromFormat('U.u', number_format(microtime(true), 6, '.', ''));
-            $formateado = $ahora->format("Y-m-d H:i:s.u");
-            echo $formateado; //date("Y-m-d H:i:s.u"); 
-            echo chr(10);
+            
         }
     } else {
         foreach ($adir as $ruta) {
@@ -179,41 +160,50 @@ function dividerPdf($path2, $pdfPath, $pdfAbsolutePath)
 {
     $rut = $GLOBALS["rut"];
     $length = $GLOBALS["length"];
-    
-    $string1 = substr(str_repeat(0, $length) . '1', -$length);
-    $new_filename2 = $pdfPath . $string1 . ".pdf";  //_P00001
 
-    
-    $paso = file_exists($new_filename2);
-
-    if ($paso) {
-        echo '-Archivo ya procesado-';
-    } else {
-
-        $pdf = new Fpdi();
-        $pagecount = $pdf->setSourceFile($pdfAbsolutePath);
-
-        if ($pagecount) {
-            if ($pagecount > 1) {
-                // Split each page into a new PDF
-                for ($i = 1; $i <= $pagecount; $i++) {
-                    $string = substr(str_repeat(0, $length) . $i, -$length);
-                    $new_filename2 = $pdfPath . $string . ".pdf";
-                    $new_pdf = new Fpdi();
-                    $new_pdf->AddPage();
-                    $new_pdf->setSourceFile($pdfAbsolutePath);
-                    $new_pdf->useTemplate($new_pdf->importPage($i));
-                    try {
-                        $new_filename = $new_filename2; 
-                        $new_pdf->Output($new_filename, "F");
-                    } catch (Exception $e) {
-                        echo 'Caught exception: ',  $e->getMessage(), "\n";
-                    }
+    $pdf = new Fpdi();
+    $pagecount = $pdf->setSourceFile($pdfAbsolutePath);
+    echo $pagecount . ' pág ';
+    if ($pagecount) {
+        if ($pagecount > 1) {
+            $fecha = new DateTime();
+            $nstamp = $fecha->getTimestamp();
+            $ahora = DateTime::createFromFormat('U.u', number_format(microtime(true), 6, '.', ''));
+            $formateado = $ahora->format("Y-m-d H:i:s.u");
+            echo 'Inicia division' . $formateado . ' -> ';
+            // Split each page into a new PDF
+            for ($i = 1; $i <= $pagecount; $i++) {
+                $string = substr(str_repeat(0, $length) . $i, -$length);
+                $new_filename2 = $pdfPath . $string . ".pdf";
+                $new_pdf = new Fpdi();
+                $new_pdf->AddPage();
+                $new_pdf->setSourceFile($pdfAbsolutePath);
+                $new_pdf->useTemplate($new_pdf->importPage($i));
+                try {
+                    $new_filename = $new_filename2;
+                    //$new_pdf->Output($new_filename, "F");
+                    //obtenemos mdf5 y timestamp de la hoja
+                    $file_content = $new_pdf->Output("S",$new_filename, false);
+                    $md5f = md5($file_content);
+                    $cadMd5ts = cadstamtime($md5f, $i);
+                    //agregamos MD5 + timespamt + hoja
+                    $new_pdf->SetFont('Arial', 'B', '11');
+                    $new_pdf->SetXY(30,-15);
+                    $new_pdf->Write(200,$cadMd5ts);
+                    //guardamos
+                    $new_pdf->Output($new_filename, "F");
+                } catch (Exception $e) {
+                    echo 'Caught exception: ',  $e->getMessage(), "\n";
                 }
             }
+            $fecha = new DateTime();
+            $nstamp2 = $fecha->getTimestamp();
+            $ahora = DateTime::createFromFormat('U.u', number_format(microtime(true), 6, '.', ''));
+            $formateado = $ahora->format("Y-m-d H:i:s.u");
+            $difstamp = $nstamp2 - $nstamp;
+            echo $formateado.' = '.$difstamp.chr(10); 
         }
     }
-    //}
 }
 
 function verifyPdfVersion($pdfPath)
@@ -239,13 +229,13 @@ function verifyPdfVersion($pdfPath)
 
             //if (!file_exists(dirname($newPdfVersionPath)))
 
-                //mkdir(dirname($newPdfVersionPath), 0777, true);
+            //mkdir(dirname($newPdfVersionPath), 0777, true);
 
-                // USE GHOSTSCRIPT IF PDF VERSION ABOVE 1.4 AND SAVE ANY PDF TO VERSION 1.4 , SAVE NEW PDF OF 1.4 VERSION TO NEW LOCATION
-                //logger('gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -sOutputFile="' . $newPdfVersionPath . '" "' . $pdfPath . '"');
+            // USE GHOSTSCRIPT IF PDF VERSION ABOVE 1.4 AND SAVE ANY PDF TO VERSION 1.4 , SAVE NEW PDF OF 1.4 VERSION TO NEW LOCATION
+            //logger('gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -sOutputFile="' . $newPdfVersionPath . '" "' . $pdfPath . '"');
 
 
-                $ahora = DateTime::createFromFormat('U.u', number_format(microtime(true), 6, '.', ''));
+            $ahora = DateTime::createFromFormat('U.u', number_format(microtime(true), 6, '.', ''));
             $formateado = $ahora->format("Y-m-d H:i:s.u");
             echo 'conversion ' . $formateado . ' --> ';
 
@@ -308,5 +298,10 @@ function isActiveGS()
     return shell_exec("gs -v");
 }
 
+function cadstamtime($md5actual, $hoja)
+{
+    $hoy = time();
+    return $md5actual.' '.date("Y-m-d H:I:s",$hoy).' page ['.$hoja.']';
+}
 
 ?>
